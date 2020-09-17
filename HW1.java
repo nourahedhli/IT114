09c4576ebc106b18c43099badd5a7e3dda516179
nourahedhli <nh98@njit.edu>
2020-09-17 13:53:45 -0400

    JAVE HW1

---
+++ HW1.java
@@ -0,0 +1,18 @@
+public class Homework{
+
+     public static void main(String []args){
+        // If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+        String name = "Noura";
+		// 2. Output the name to the console
+        System.out.println(name);
+		// 3. Create a variable that holds an age (as a whole number)
+        int age = 24;
+		// 4. Output the age to the console
+    System.out.println(age);
+		// 5. Create a variable that holds a decimal
+        float dec = 2.2f;
+		// 6. Output the variable
+		System.out.println(dec);
+     }
+}
